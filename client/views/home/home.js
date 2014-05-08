2154ddfdbd40da81f61c7ca5c6551c4a9facfdd1
Denny Scott <dennyscott@Dennys-MacBook-Pro.local>
2014-05-07 20:15:57 -0500

    Added new boilerplate code

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,19 +1,12 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
+
+  Template.home.greeting = function () {
     return "Welcome to zephyrware.";
   };
 
-  Template.hello.events({
+  Template.home.events({
     'click input': function () {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
     }
   });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
