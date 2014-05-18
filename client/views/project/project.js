b8d047c2a5153c453ebacecedb292b43b9c8c7b7
Denny Scott <DennyScott301@gmail.com>
2014-05-18 16:55:22 -0500

    Moved all side bar and page layout to loggedIn template

--- client/views/project/project.js
+++ client/views/project/project.js
@@ -1,38 +1,3 @@
-var sliding = false;
-
 Template.project.rendered = function(){
 
 };
-
-Template.project.events({
-    'mouseenter .launch.attached' : function(event){
-        var button = $(event.currentTarget);
-        $(button).animate({
-            width: '170px'
-        }, 300, function(){
-            button.find('.text').show();
-        });
-    },
-
-    'click #menu' : function(){
-
-        var $sidebar = $('.ui.sidebar');
-        var $launchButton = $('.launch.attached');
-
-        $sidebar.sidebar({
-        }).sidebar('toggle');
-
-
-        $launchButton.toggleClass('pushed');
-
-
-    },
-
-    'mouseleave .launch.attached' : function(event){
-        var button = $(event.currentTarget);
-        button.find('.text').hide();
-        button.stop().animate({
-            width: '70px'
-        }, 300);
-    }
-});
---
+++ client/views/templates/loggedIn/loggedIn.js
@@ -0,0 +1,33 @@
+Template.loggedIn.events({
+    'mouseenter .launch.attached' : function(event){
+        var button = $(event.currentTarget);
+        $(button).animate({
+            width: '170px'
+        }, 300, function(){
+            button.find('.text').show();
+        });
+    },
+
+    'click #menu' : function(){
+
+        var $sidebar = $('.ui.sidebar');
+        var $launchButton = $('.launch.attached');
+
+        $sidebar.sidebar({
+            overlay:true
+        }).sidebar('toggle');
+
+
+        $launchButton.toggleClass('pushed');
+
+
+    },
+
+    'mouseleave .launch.attached' : function(event){
+        var button = $(event.currentTarget);
+        button.find('.text').hide();
+        button.stop().animate({
+            width: '70px'
+        }, 300);
+    }
+});
