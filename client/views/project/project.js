7c8b7388107ead94eeb9a815004250dbd3438167
Gizmmo <travisscott301@gmail.com>
2014-05-18 21:21:14 -0500

    Editing Sortables

--- client/views/project/project.js
+++ client/views/project/project.js
@@ -1,44 +1,55 @@
 Template.project.rendered = function(){
+    // $( ".scrumColumn" ).sortable({
+    //     connectWith: ".scrumColumn",
+    //     handle: ".portlet-content",
+    //     cancel: ".portlet-toggle",
+    //     start: function (event, ui) {
+    //         ui.item.addClass('tilt');
+    //         tilt_direction(ui.item);
+    //     },
+    //     stop: function (event, ui) {
+    //         ui.item.removeClass("tilt");
+    //         $("html").unbind('mousemove', ui.item.data("move_handler"));
+    //         ui.item.removeData("move_handler");
+    //     }
+    // }).disableSelection();
+
+// function tilt_direction(item) {
+//     var left_pos = item.position().left,
+//         move_handler = function (e) {
+//             if (e.pageX >= left_pos) {
+//                 item.addClass("right");
+//                 item.removeClass("left");
+//             } else {
+//                 item.addClass("left");
+//                 item.removeClass("right");
+//             }
+//             left_pos = e.pageX;
+//         };
+//     $("html").bind("mousemove", move_handler);
+//     item.data("move_handler", move_handler);
+// }
+// 
+$(function() {
     $( ".scrumColumn" ).sortable({
-    connectWith: ".scrumColumn",
-    handle: ".portlet-header",
-    cancel: ".portlet-toggle",
-    start: function (event, ui) {
-        ui.item.addClass('tilt');
-        tilt_direction(ui.item);
-    },
-    stop: function (event, ui) {
-        ui.item.removeClass("tilt");
-        $("html").unbind('mousemove', ui.item.data("move_handler"));
-        ui.item.removeData("move_handler");
-    }
-});
+      connectWith: ".scrumColumn",
+      handle: ".portlet-header",
+       cancel: ".portlet-toggle",
+      placeholder: "portlet-placeholder ui-corner-all"
+  });
 
-function tilt_direction(item) {
-    var left_pos = item.position().left,
-        move_handler = function (e) {
-            if (e.pageX >= left_pos) {
-                item.addClass("right");
-                item.removeClass("left");
-            } else {
-                item.addClass("left");
-                item.removeClass("right");
-            }
-            left_pos = e.pageX;
-        };
-    $("html").bind("mousemove", move_handler);
-    item.data("move_handler", move_handler);
-}
-
-$( ".portlet" )
+    $( ".portlet" )
     .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
     .find( ".portlet-header" )
     .addClass( "ui-widget-header ui-corner-all" )
     .prepend( "<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
 
-$( ".portlet-toggle" ).click(function() {
-    var icon = $( this );
-    icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
-    icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
+    $( ".portlet-toggle" ).click(function() {
+      var icon = $( this );
+      icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
+      icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
+  });
 });
+
+
 };
