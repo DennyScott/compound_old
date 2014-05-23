1a59929b7d2bd94b8ba34a3046b2945210d9ec28
Gizmmo <travisscott301@gmail.com>
2014-05-22 19:13:57 -0500

    Working on hover

--- client/views/modal/task.js
+++ client/views/modal/task.js
@@ -8,6 +8,32 @@ Template.task.rendered = function () {
     on: 'click'
   });
   
+//   $('.all-collabs .image')
+//   .dimmer({
+//     on: 'hover'
+//   })
+// ;
+
+//console.log($('.all-collabs.image'));
 
 
 };
+
+Template.task.events({
+	'click .card-menu': function () {
+		$('.collab-person .image')
+		  .dimmer({
+		    on: 'hover'
+		 	});
+	}
+});
+$(document).ready(function(){
+  $('.isCheck').click(function () {
+		console.log("Hi!");
+	});
+});
+
+function sayHello() {
+	console.log("I've said hello!");
+}
+
