55b0e3356b3f5e988137d5addcb6871a03032998
Gizmmo <travisscott301@gmail.com>
2014-05-26 23:39:49 -0500

    Created Basic Projects

---
+++ collections/projects/projects.js
@@ -0,0 +1,143 @@
+Projects = new Meteor.Collection('projects');
+
+Meteor.methods({
+
+	/**
+	 * Creates a new project and places it in the Projects table
+	 * @param  {[Object]} projectAttributes [A object containing a title and description]
+	 * @return {[String]}                   [ID of the new Project]
+	 */
+	 project: function(projectAttributes){
+
+		var user = Meteor.user();
+
+		//Ensures that the user is logged in
+		if (!user){
+			throw new Meteor.Error(401, "You need to log in to create new stories");
+		}
+
+		if(!projectAttributes.title){
+			throw new Meteor.Error(422, 'Error 422: Project must have a title');
+		}
+
+		//filling in other keys
+		var proj = _.extend(_.pick(storyAttributes, 'title', 'description'), {
+			authorID: user._id,
+			submitted: new Date().getTime(),
+			lastUpdated: new Date().getTime(),
+			updateAuthorID: user._id,
+			stories: [], 	// {id, submitted, lastUpdated, authorID, stage, position, scheduledDate, label[], collaborators[]}
+			iteration: [],	// {id, startDate, endDate, storyId}
+			comments: [],  	//{id, submitted, lastUpdated, authorID}
+			labels: []    	//{id, color, name}
+		});
+
+		//Inserts new project into collection
+		var projectID = Projects.insert(proj);
+
+		//returns the ID of the new project
+		return projectID;
+	},
+
+	//-----------------------------------PROJECT UPDATE METHODS----------------------------------------------//
+	
+	/**
+	 * Updates the Description of a Project
+	 * @param  {[String]} id          [The ID of the Project to update]
+	 * @param  {[String]} description [The new description of that Project]
+	 * @return {[void]}             [No Return]
+	 */
+	updateProjectDescription: function(id, description){
+		Projects.update(id, {$set: {'description': description}});
+		Meteor.call('updateProject', id);
+	},
+
+	/**
+	 * Update the update vitols of a project
+	 * @param  {[String]} id [The ID of the Project]
+	 * @return {[void]}    [No Return]
+	 */
+	updateProject: function(id) {
+		var user = Meteor.user();
+		var now = new Date().getTime;
+		Projects.update(id, {$set: {'lastUpdated': now,  'updateAuthorID': user._id}});
+	},
+
+	/**
+	 * Update the Title of a Project
+	 * @param  {String} id    The ID of the Project
+	 * @param  {String} title The New title of the Project
+	 * @return {void}       No Return
+	 */
+	updateTitle: function(id, title) {
+		Projects.update(id, {$set: {'title': title}});
+		Meteor.call('updateProject', id);
+	},
+
+	/**
+	 * Update the Author ownership of the Project
+	 * @param  {String} id     The ID of the project
+	 * @param  {String} author The ID of the author
+	 * @return {void}        No Return
+	 */
+	updateAuthor: function(id, author) {
+		Projects.update(id, {$set: {'authorID': author}});
+		Meteor.call('updateProject', id);
+	},
+
+	//---------------------------------END OF PROJECT UPDATE METHODS-----------------------------------------//
+
+	//-----------------------------------PROJECT REMOVE METHODS----------------------------------------------//
+	
+	/**
+	 * Removes a project for the projects collection
+	 * @param  {String} id The ID of the project
+	 * @return {void}    No Return
+	 */
+	removeProject: function(id) {
+		var user = Meteor.user();
+		var found = Projects.findOne(id);
+		if(found.authorID === user._id){
+			Projects.remove(id);
+		} else {
+			throw new Meteor.Error(405, "You need to own this project to delete it");
+		}
+	},
+	//---------------------------------END OF PROJECT REMOVE METHODS-----------------------------------------//
+	
+
+	addTask: function(id, task){
+		//Need to check input
+		var finalizedTask = _.extend(_.pick(task, 'title', 'description'), {
+
+		});
+		var story = Stories.findOne(id);
+		var maxID = 0;
+		story.tasks.forEach(function(entry) {
+			if(entry.id > maxID){
+				maxID = entry.id;
+			}
+		});
+		finalizedTask.id = maxID+1;
+		Stories.update(id, {$push: {'tasks': finalizedTask}});
+	},
+
+	removeTask: function(id, taskID){
+		Stories.update(id, {$pull: {'tasks' : {'id': taskID}}});
+	},
+
+	updateTask: function(id, task){
+		var story = Stories.findOne(id);
+		var tasks = story.tasks;
+		for(i = 0; i < tasks.length; i++){
+			if(tasks[i].id === task.id){
+				tasks[i] = task;
+			}
+		}
+		Stories.update(id, {$set: {'tasks' : tasks}});
+	},
+
+	removeStory: function(id){
+		Stories.remove(id);
+	}
+});
---
+++ collections/tasks.js
@@ -0,0 +1,100 @@
+Tasks = new Meteor.Collection('tasks');
+
+Meteor.methods({
+	/**
+	 * This function will data validate the new story being passed
+	 * and then if no errors occur, place this new story in
+	 * the Story collection
+	 * @param  {[object]} storyAttributes [A prelimeanry story object, containg the title, description, and url]
+	 * @return {[string]}                   [A String of its ID in the collection]
+	 */
+	 task: function(taskAttributes){
+
+		// var user = Meteor.user();
+		// var userName = Meteor.user().profile.firstName + " " + Meteor.user().profile.lastName;
+
+		// //Ensures that the user is logged in
+		// if (!user){
+		// 	throw new Meteor.Error(401, "You need to log in to create new stories");
+		// }
+
+		if(!taskAttributes.title){
+			throw new Meteor.Error(422, 'Error 422: Task must have a title');
+		}
+
+		//filling in other keys
+		var finalTask = _.extend(_.pick(taskAttributes, 'title', 'description', 'storyID'), {
+			// authorID: user._id,
+			// authorName: userName,
+			submitted: new Date().getTime(),
+			lastUpdated: new Date().getTime(),
+			checklist: [],
+			comments: [],
+			attachments: [],
+			ratings: [],
+			scheduledDate: []
+		});
+
+		//Inserts new story into collection
+		var taskID = Tasks.insert(finalTask);
+		Meteor.call('updateStoryDate', finalTask.storyID);
+		//returns the ID of the new story
+		return taskID;
+	}
+	// },
+
+	// addChecklistItem: function(id, listItem){
+	// 	var item = {
+	// 		title: listItem.title,
+	// 		isChecked: false
+	// 	};
+	// 	var task = Stories.findOne(id);
+	// 	var maxID = 0;
+	// 	story.tasks.forEach(function(entry) {
+	// 		if(entry.id > maxID){
+	// 			maxID = entry.id;
+	// 		}
+	// 	});
+	// 	finalizedTask.id = maxID+1;
+	// 	Tasks.update(id, {$push: {'checklist': item}});
+	// },
+
+	// updateChecklist: function(id, list){
+	// 	var found = Tasks.findOne(id);
+	// 	found.checklist = list;
+	// 	Tasks.update(id, found);
+	// },
+
+	// addComment: function(id, comment){
+	// 	var finalComment = {
+	// 		comment: comment.comment,
+	// 		authorName: authorName,
+	// 		authorID: authorID,
+	// 		submitted: new Date().getTime(),
+	// 		lastUpdated: new Date().getTime(),
+	// 		likes: 0
+	// 	};
+	// 	Tasks.update(id, {$push: {'comments': finalComment}});
+	// },
+
+	// addRating: function(id, rating){
+	// 	var finalRating = {
+	// 		amount: rating.amount,
+	// 		title: rating.title,
+	// 		authorID: authorID,
+	// 		authorName: authorName,
+	// 		submitted: new Date().getTime(),
+	// 		lastUpdated: new Date().getTime()
+	// 	};
+	// 	Tasks.update(id, {$push: {'ratings': finalRating}});
+	// },
+
+	// removeTask: function(id){
+	// 	Tasks.remove(id);
+	// },
+
+	// updateTask: function(task) {
+	// 	task.lastUpdated = new Date().getTime;
+	// 	Tasks.update(id, task);
+	// }
+});
