c1ed47f122580fd41dbc23040faa42cfc9c81883
Gizmmo <travisscott301@gmail.com>
2014-10-09 20:27:51 -0500

    Fixed card movement

--- client/views/project/project.js
+++ client/views/project/project.js
@@ -1,25 +1,6 @@
 var project;
 Template.project.rendered = function() {
 
-    $('.scrumInner').sortable().disableSelection();
-
-    $(".scrumColumn").sortable({
-        connectWith: '.scrumColumn',
-        placeholder: 'portlet-placeholder ui-corner-all',
-        cancel: '.add-card'
-    }).disableSelection();
-
-    $(".portlet")
-    .addClass("ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
-    .find(".portlet-header")
-    .prepend("<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
-
-    $(".portlet-toggle").click(function() {
-        var icon = $(this);
-        icon.toggleClass("ui-icon-minusthick ui-icon-plusthick");
-        icon.closest(".portlet").find(".portlet-content").toggle();
-    });
-
 
 };
 
